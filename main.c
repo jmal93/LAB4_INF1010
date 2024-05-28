54774a39c8267344b7ebdd57d44a94225e66492c
João Marcello <131413084+jmal93@users.noreply.github.com>
2024-05-28 10:50:02 +0000

    funções de busca e criação de nó criadas

--- main.c
+++ main.c
@@ -12,8 +12,52 @@ struct no
 };
 
 void intervalo(t_no *arv, int lim_inf, int lim_sup);
+t_no* busca(t_no *arv, int chave);
+t_no* criaNo(void);
+t_no* insere(t_no* arv, int chave);
 
 int main(void) {
+	t_no* arvore = criaNo();
+	busca(arvore, 5);
 	return 0;
 }
 
+// cria um nó vazio
+t_no* criaNo(void)
+{
+	t_no* novoNo = (t_no*) malloc(sizeof(t_no));
+	novoNo->ndesc = 0;
+	for (int i = 0; i < MAX + 1; i++)
+		novoNo->ramo[i] = NULL;
+	return novoNo;
+}
+
+// retorna a página na qual a chave foi encontrada, retorna nulo caso não ache
+t_no* busca(t_no *arv, int chave)
+{
+	if (arv == NULL)
+		return NULL;
+	for (int i = 0; i < MAX; i++)
+	{
+		if (arv->chave[i] == chave)
+			return arv;
+	}
+
+	int j = 0;
+	if (chave < arv->chave[j])
+		busca(arv->ramo[j], chave);
+	for (j = 1; j < MAX; j++)
+	{
+		if (arv->chave[j] < chave && chave < arv->chave[j + 1])
+			busca(arv->ramo[j], chave);
+	}
+	if (arv->chave[j] < chave)
+		busca(arv->ramo[j], chave);
+
+	return NULL;
+}
+
+t_no* insere(t_no* arv, int chave)
+{
+	
+}
